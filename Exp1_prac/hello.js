af3472319b5e3f348617bd200e10916b2dc279a6
Ayesha Saifi <ayeshaas281@gmail.com>
2026-09-08 11:24:14 +0530

    exp 1 practise

--- Exp1_prac/hello.js
+++ Exp1_prac/hello.js
@@ -1,5 +1,5 @@
-import { add } from "./add.js";
-import { sub } from "./sub.js";
+import { Add } from "./add.js";
+import { Sub } from "./sub.js";
 
-console.log("Addition:", add(10, 5));
-console.log("Subtraction:", sub(10, 5));
+console.log("Addition:", Add(10, 5));
+console.log("Subtraction:", Sub(10, 5));
